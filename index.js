9745ef4dc3899f443c9de1fab139df6577170401
Vineet Dev <Vineet.Dev@ril.com>
2017-05-16 00:10:12 +0530

    port corrected
    Signed-off-by: Vineet Dev <Vineet.Dev@ril.com>

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 var express = require('express')
 var app = express();
 
+var port = process.env.PORT || 5000;
+
 process.env.PWD = process.cwd();
 
 app.use(express.static(process.env.PWD + '/'));
 
 app.get('*', function (req, res) {
-  res.sendfile(process.env.PWD + '/index.html');
+  res.render(process.env.PWD + '/index.html');
 })
 
-app.listen(5000, function() {
-  console.log("running app on 5000 port");
+app.listen(port, function() {
+  console.log("running app on " + port + " port");
 })
